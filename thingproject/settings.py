61bacd71cd7d641eef952a0396d2750b0a0f48b6
Michael Seiranian <michaelseiranian@gmail.com>
2022-10-18 00:21:22 +0100

    Refine Thing model

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -124,6 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Thing model for authentication
-AUTH_USER_MODEL = 'things.Thing'
