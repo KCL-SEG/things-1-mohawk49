2c552b3ae213bb9e58c8103ae0de48ccf95fead5
Michael Seiranian <michaelseiranian@gmail.com>
2022-10-18 00:26:46 +0100

    Error correction TextField to CharField for description

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     REQUIRED_FIELDS = ('name',)
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(max_length=120, unique=False, blank=True)
+    description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)], unique=False)
