13dbbae2a49a16a25474dcfd3f93a44c36a46fa6
Michael Seiranian <michaelseiranian@gmail.com>
2022-10-17 19:49:12 +0100

    Error correction

--- things/views.py
+++ things/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    response = HttpResponse('Welcome to things!')
-    return response
+    return render(request, 'home.html')
 # Create your views here.
